9333882445dded349a507b259afb358dbfa0631a
pkhruasuwan@ebay.com <pkhruasuwan@ebay.com>
2018-04-20 14:09:29 -0700

    Second commit

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -12,10 +12,15 @@ class TodoListViewController: UITableViewController {
 
     var itemArray = ["Find mike", "Buy Games", "Destroy this reality"]
     
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     
+        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
+            itemArray = items
+        }
 //        itemTableView.delegate = self
 //        itemTableView.dataSource = self
     }
@@ -67,6 +72,9 @@ class TodoListViewController: UITableViewController {
             (action) in
             
             self.itemArray.append(textField.text ?? "New Item")
+            
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+            
             self.tableView.reloadData()
         }
         
